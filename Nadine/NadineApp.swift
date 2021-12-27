38f04ee07c2ff153a1be56f22ebef8298cc0b1a2
Sean Blake <sean@fiv4lab.com>
2021-12-27 09:39:35 -0500

    Initial Commit

---
+++ Nadine/NadineApp.swift
@@ -0,0 +1,17 @@
+//
+//  NadineApp.swift
+//  Nadine
+//
+//  Created by SEAN BLAKE on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct NadineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
