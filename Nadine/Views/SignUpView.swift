b91b5d90a8b63e674bf73ee9440713fcfe09d98c
Sean Blake <sean.blake97@outlook.com>
2022-02-27 12:39:12 -0500

    create custom modifiers for the login button

--- Nadine/Views/SignUpView.swift
+++ Nadine/Views/SignUpView.swift
@@ -20,7 +20,9 @@ struct SignUpView: View {
                 Text("Create a account")
                     .frame(maxWidth: .infinity)
             }
-            .buttonStyle(.bordered)
+            .font(.headline)
+            .blendMode(.overlay)
+            .buttonStyle(.angular)
             .tint(.accentColor)
             .controlSize(.large)
         }
@@ -37,5 +39,7 @@ struct SignUpView: View {
 struct SignUpView_Previews: PreviewProvider {
     static var previews: some View {
         SignUpView()
+            
+
     }
 }
