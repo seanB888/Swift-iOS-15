a6417f23bf0bc872d9c49612b6e2e903d36655f9
Sean Blake <sean.blake97@outlook.com>
2022-02-27 14:38:33 -0500

    Adding custom focus to the fields for email and password

--- Nadine/Views/SignUpView.swift
+++ Nadine/Views/SignUpView.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct SignUpView: View {
+    enum Field: Hashable {
+        case email
+        case password
+    }
+
     @State var email = ""
     @State var password = ""
+    @FocusState var focusedField: Field?
+    @State var circleY: CGFloat = 120
 
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
@@ -17,7 +24,7 @@ struct SignUpView: View {
                 .font(.largeTitle).bold()
             Text("Access 120+ hours of courses, \ntutorials and livestreams")
                 .font(.headline)
-            
+
             // email address field
             TextField("Email", text: $email)
                 .inputStyle(icon: "mail")
@@ -25,11 +32,15 @@ struct SignUpView: View {
                 .keyboardType(UIKeyboardType.emailAddress)
                 .autocapitalization(.none)
                 .disableAutocorrection(true)
+                .focused($focusedField, equals: .email)
+                .shadow(color: focusedField == .email ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
 
             // password field
             SecureField("Password", text: $password)
                 .inputStyle(icon: "lock")
                 .textContentType(.password)
+                .focused($focusedField, equals: .password)
+                .shadow(color: focusedField == .password ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
 
             // submit button
             Button {} label: {
@@ -64,21 +75,34 @@ struct SignUpView: View {
         }
         .padding(20)
         .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .background(
+            Circle().fill(.blue)
+                .frame(width: 68, height: 68)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .offset(y: circleY)
+        )
         .strokeStyle(cornerRadius: 30)
         .shadow(color: Color("Shadow").opacity(0.2), radius: 30, x: 0, y: 30)
+        .padding(.horizontal, 20)
         .background(
             Image("Blob 1").offset(x: 200, y: -100)
         )
-        .padding(.horizontal, 20)
+        .onChange(of: focusedField) { value in
+            withAnimation {
+                if value == .email {
+                    circleY = 120
+                } else {
+                    circleY = 190
+                }
+            }
+        }
     }
 }
 
 struct SignUpView_Previews: PreviewProvider {
     static var previews: some View {
-        SignUpView()
-
-
-
-
+        ZStack {
+            SignUpView()
+        }
     }
 }
