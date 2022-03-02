55517fbe781b1b59af348b2d0e570e37c9400153
Sean Blake <sean.blake97@outlook.com>
2022-03-02 16:45:07 -0500

    Create PlayButton, and the ExploreView

---
+++ Nadine/Components/CircularView.swift
@@ -0,0 +1,36 @@
+//
+//  CircularView.swift
+//  Nadine
+//
+//  Created by SEAN BLAKE on 3/1/22.
+//
+
+import SwiftUI
+
+struct CircularView: View {
+    var value: CGFloat = 0.2
+    var lineWidth: Double = 4
+    @State var appear = false
+
+    var body: some View {
+        Circle()
+            .trim(from: 0, to: value)
+            .stroke(style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
+            .fill(.angularGradient(colors: [.purple, .pink, .purple], center: .center, startAngle: .degrees(0), endAngle: .degrees(360)))
+            .rotationEffect(.degrees(270))
+            .onAppear {
+                withAnimation(.spring().delay(0.5)) {
+                    appear = true
+                }
+            }
+            .onDisappear {
+                appear = false
+            }
+    }
+}
+
+struct CircularView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularView()
+    }
+}
---
+++ Nadine/Views/ExploreView.swift
@@ -0,0 +1,41 @@
+//
+//  ExploreView.swift
+//  Nadine
+//
+//  Created by SEAN BLAKE on 3/2/22.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        ZStack {
+            Color("Background").ignoresSafeArea()
+
+            coursesSection
+            .safeAreaInset(edge: .top) {
+                Color.clear.frame(height: 70)
+            }
+            .overlay(NavigationBar(title: "Recent", hasScrolled: .constant(true)))
+            .background(Image("Blob 1").offset(x: -100, y: -400))
+        }
+    }
+
+    var coursesSection: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 16) {
+                ForEach(courses) { course in
+                    SmallCourseItem(course: course)
+                }
+            }
+            .padding(.horizontal, 20)
+            Spacer()
+        }
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
